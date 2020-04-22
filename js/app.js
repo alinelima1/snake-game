485af8cfc329c90e5568085ebffd6b8b2dae0c85
Aline de Souza Lima Abreu <alineslima17@gmail.com>
2020-04-21 23:56:52 -0300

    end game

--- js/app.js
+++ js/app.js
@@ -7,6 +7,11 @@ snake[0] = {
     y: 8 * box
 }
 let direction="right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
 
 let criarBG = () => {
     context.fillStyle = "black";
@@ -20,9 +25,37 @@ let criarCobrinha = () => {
     }
 }
 
+let criarComida = () => {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box)
+}
+
+let update = (event) => {
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+document.addEventListener('keydown', update);
+
 let iniciarJogo = () => {
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Fim de jogo!")
+        }
+    }
+
+
     criarBG();
     criarCobrinha();
+    criarComida();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -32,13 +65,23 @@ let iniciarJogo = () => {
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }  
 
     let newHead = {
         x: snakeX,
         y: snakeY
     }
+
+    snake.unshift(newHead);
 }
 
-let jogo = setInterval(iniciarJogo, 100);
+let jogo = setInterval(iniciarJogo, 300);
 
+let restart = () => {
+    location.reload({ignoreLoading : true});
+}
